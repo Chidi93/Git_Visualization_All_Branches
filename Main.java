73e606a63f54082f8574fc0835bbecfb1d21da71
Chidi Agbo <chidi.agbo93@gmail.com>
2020-10-11 17:35:20 -0600

    revision three (in master)

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
 public class Main{
 
     public static void main(String[] args) {
-        //This is the chronolgical revision 1 in master
+        //This is the chronolgical revision 3 in master
         
      System.out.println("Hello World");
         
+    }
+
+    public void methodInMaster(){
+
+        System.out.println("Method in Master");
     }
 }
